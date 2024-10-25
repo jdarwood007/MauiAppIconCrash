9e4b148a80560a7ea40c54879de59750c8a383e2
Jeremy <jeremy@auvilfruit.com>
2024-10-25 13:23:14 -0700

    Remove Mvvm as a test

--- MauiApp6/AddViewModel.cs
+++ MauiApp6/AddViewModel.cs
@@ -1,18 +1,23 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiApp6;
-public partial class AddViewModel : ObservableObject
+public partial class AddViewModel : INotifyPropertyChanged
 {
     protected const string ButtonEmpty = "button_empty";
     protected const string ButtonNote = "button_note";
 
-    [ObservableProperty]
     private string? noteIcon = ButtonEmpty;
+    public string NoteIcon
+    {
+        get => noteIcon ?? ButtonEmpty;
+        set => SetProperty(ref noteIcon, value);
+    }
 
     public void SetIcon()
     {
@@ -28,4 +33,41 @@ public partial class AddViewModel : ObservableObject
             Console.WriteLine(e);
         }
     }
+
+    protected bool SetProperty<T>(ref T backingStore, T value,
+    [CallerMemberName] string propertyName = "",
+    Action? onChanged = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(backingStore, value))
+        {
+            return false;
+        }
+
+        backingStore = value;
+        onChanged?.Invoke();
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    #region INotifyPropertyChanged
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        try
+        {
+            PropertyChangedEventHandler? changed = PropertyChanged;
+            if (changed == null)
+            {
+                return;
+            }
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+    #endregion
+
 }
